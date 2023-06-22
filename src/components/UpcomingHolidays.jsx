cf741ecb94d1d1c9e547987cd97021b0133334bb
enohyn <shayokullah@gmail.com>
2023-06-22 15:46:04 +0600

    card size maintained

--- src/components/UpcomingHolidays.jsx
+++ src/components/UpcomingHolidays.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const UpcomingHolidays = () => {
     return (
-        <section className='w-100 d-flex flex-column px-2 column-gap-2 '>
+        <section className='w-100 d-flex flex-column px-2 gap-2  '>
             <h3>
-                Upcoming
+                Upcoming Holidays
             </h3>
-            <div className='row d-flex flex-column flex-md-row justify-content-around align-items-center w-100 '>
+            <div className='row d-flex flex-column flex-md-row justify-content-around align-items-center w-100 px-3'>
                 <div class="card col-12 col-md-6 col-lg-3 text-bg-light mb-3" style={{maxWidth:"18rem", height:"10rem"}}>
                     <h3 class="card-header">Independence Day</h3>
                     <div class="card-body">
